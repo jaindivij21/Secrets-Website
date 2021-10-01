45da0f3a8a231ea411d7c1e556c34df4fdafb1a7
Divij Jain <jaindivijwork@gmail.com>
2021-10-01 20:53:04 +0530

    App Setup

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-//jshint esversion:6
+//* MAIN app.js
+
+//* Requirements
+/* #region */
+const express = require("express");
+const https = require("https");
+const mongoose = require("mongoose");
+const _ = require("lodash");
+/* #endregion */
+
+//* App Constant
+/* #region */
+// get APP CONST, setup static files, initialize ejs and body parser
+const app = express();
+app.use(express.static(__dirname + "/public"));
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+/* #endregion */
+
+//* MAIN
+
+//* Setup Database
+
+//* Pages
+/* #region */
+
+// Listen to browser port
+app.listen(process.env.PORT || 3000, () => {
+    console.log("Server Started!");
+});
+
+/* #endregion */
